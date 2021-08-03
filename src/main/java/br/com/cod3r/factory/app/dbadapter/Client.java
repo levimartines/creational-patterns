dfccaf4039fb3028895eff820048d4e2760af08e
Levi Martines <levi.mferreira@gmail.com>
2021-08-02 23:00:01 -0300

    implementando padrão factory na criação da conexão de dados

--- src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,16 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.OracleDBFactory;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresDBFactory;
+
 public class Client {
 
 	public static void main(String[] args) {
+		//DB db = new OracleDBFactory().getDatabase();
+		DB db = new PostgresDBFactory().getDatabase();
+		db.query("SELECT * FROM users");
+		db.update("UPDATE users SET nome='Pedro' WHERE id=5");
 
 	}
 }
