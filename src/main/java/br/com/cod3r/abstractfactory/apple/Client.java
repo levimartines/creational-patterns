f0f4f256a61076d7abfeaa347f66d396fd186dc0
Levi Martines <levi.mferreira@gmail.com>
2021-10-11 23:02:06 -0300

    Implementing the Abstract Factory pattern

--- src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,27 @@
 package br.com.cod3r.abstractfactory.apple;
 
-public class Client {
-	
-	public static void main(String[] args) {
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
 
-	}
+public class Client {
+
+    public static void main(String[] args) {
+        //CountryRulesAbstractFactory rules = new USRulesFactory();
+        CountryRulesAbstractFactory rules = new BrazilianRulesFactory();
+
+        IPhoneFactory iPhone11Factory = new IPhone11Factory(rules);
+        IPhoneFactory iPhoneXFactory = new IPhoneXFactory(rules);
+
+        System.out.println("### Ordering an iPhone 11");
+        IPhone iPhone11 = iPhone11Factory.orderIPhone("standard");
+        System.out.println(iPhone11);
+
+        System.out.println("### Ordering an iPhone X");
+        IPhone iPhoneX = iPhoneXFactory.orderIPhone("standard");
+        System.out.println(iPhoneX);
+    }
 }
